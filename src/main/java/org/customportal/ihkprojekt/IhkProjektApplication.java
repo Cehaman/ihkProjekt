630251737e9376f235498cacb7cfe22425d8fd85
Cehaman <ch.hamann@outlook.com>
2024-01-31 00:44:50 +0100

    hhuhu

---
+++ src/main/java/org/customportal/ihkprojekt/IhkProjektApplication.java
@@ -0,0 +1,13 @@
+package org.customportal.ihkprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IhkProjektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IhkProjektApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/customportal/ihkprojekt/IhkProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package org.customportal.ihkprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IhkProjektApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
